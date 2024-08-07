3348693a8f4787494938d9f8b37825b3d4fd2dc8
Mauricio Godoy <100785540+mgodoyst@users.noreply.github.com>
2024-08-07 12:50:19 -0300

    feat: mostrar caracteristicas princiaples

--- src/app/models/pokemon.dto.ts
+++ src/app/models/pokemon.dto.ts
@@ -3,8 +3,37 @@ export class PokemonDto {
   url?: string;
   id?: number;
   sprites?: Sprites;
+  weight?: number;
+  height?: number;
+  types?: TypeContainer[];
+  abilities?: AbilityContainer[];
+  moves?: MovesContainer[];
+  description?: string;
 }
-
 class Sprites {
   front_default?: string;
+}
+
+class TypeContainer {
+  type?: Type;
+}
+
+class Type {
+  name?: string;
+}
+
+class AbilityContainer {
+  ability?: Ability;
+}
+
+class Ability {
+  name?: string;
+}
+
+class MovesContainer {
+  move?: Move;
+}
+
+class Move {
+  name?: string;
 }
--- src/app/modules/pokemon/components/pokemon/pokemon.component.ts
+++ src/app/modules/pokemon/components/pokemon/pokemon.component.ts
@@ -9,4 +9,19 @@ export class PokemonComponent {
 
   @Input() pokemon!: PokemonDto
 
+  get types() {
+    return this.pokemon.types?.map(item => item.type?.name).join(' - ');
+  }
+
+  get abilities() {
+    return this.pokemon.abilities?.map(item => item.ability?.name).join(' - ');
+  }
+
+  get weight() {
+    return (this.pokemon?.weight || 0) / 10 + ' kg';
+  }
+
+  get height() {
+    return (this.pokemon?.height || 0) / 10 + ' m';
+  }
 }
